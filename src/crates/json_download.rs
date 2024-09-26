f227781e52f5782cb7e2dfb54f4d191a4e22e41d
FlexTape <aronbeckman335@gmail.com>
2024-09-26 22:29:44 +0200

    added more comments in json_download.rs

--- src/crates/json_download.rs
+++ src/crates/json_download.rs
@@ -4,16 +4,16 @@ use std::fs::File;
 use std::io::Write;
 
 #[derive(Serialize)]
-pub struct ResultItem {
+pub struct ResultItem { // public struct which has 2 public fields type: String
     pub tag: String,
     pub text: String,
 }
 
-pub fn save_to_json(results: &[ResultItem], filename: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let json = serde_json::to_string_pretty(results)?;
+pub fn save_to_json(results: &[ResultItem], filename: &str) -> Result<(), Box<dyn std::error::Error>> { // does stuff
+    let json = serde_json::to_string_pretty(results)?; // makes it "pretty"
 
-    let mut file = File::create(filename)?;
-    file.write_all(json.as_bytes())?;
+    let mut file = File::create(filename)?; // creates a new file with the specified filename
+    file.write_all(json.as_bytes())?; // writes it to the json converted to bytes
 
     Ok(())
 }
