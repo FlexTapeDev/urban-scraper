27509ab57f8c035b27c8f08f392197beea2ca546
FlexTape <aronbeckman335@gmail.com>
2024-09-27 08:28:51 +0200

    Made the user be able to input the link themselfs.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use reqwest::blocking::get;
 use scraper::{Html, Selector};
 use std::error::Error;
-use std::io;
+use std::io::{self, Write};
 
 mod crates;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://www.rust-lang.org/"; // currently static url will change later
+    println!("Link to scrape: ");
+    let mut url = String::new(); // currently static url will change later
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut url)?;
+    let mut url = url.trim().to_string();
+
+    if !url.starts_with("https://") && !url.starts_with("http://") {
+        url = format!("https://{}", url);
+        println!("Added https://");
+    }
+
     let response = get(url)?.text()?; // get response from site that is being scraped
     let document = Html::parse_document(&response); // parse the data to be able to use it
     let selector = Selector::parse("h1, p").unwrap(); // which tags should be scraped
-
+    
     let mut results = Vec::new(); // initilizing a vector to put tag and text in
 
     for element in document.select(&selector) { // for loop searching for tags in the elements
@@ -36,4 +46,11 @@ TODO
 - Make it go on other links and scrape them too
 - Make it parse the text in the json nicer
 - Add more features
+
+
+GIT
+- git status
+- git add .
+- git commit -m "comment"
+- git push origin main
 */
