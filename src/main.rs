abde40b1838f00499eb7b3ed98bd91f6c09b9e06
FlexTape <145163781+FlexTapeDev@users.noreply.github.com>
2024-12-14 17:00:23 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let response = get(url)?.text()?; // get response from site that is being scraped
     let document = Html::parse_document(&response); // parse the data to be able to use it
-    let selector = Selector::parse("h1, p").unwrap(); // which tags should be scraped
+    let selector = Selector::parse("h1, p").unwrap(); 
     let mut results = Vec::new(); // initilizing a vector to put tag and text in
 
     for element in document.select(&selector) { // for loop searching for tags in the elements
         let tag = element.value().name().to_string();
-        let text = element.text().collect::<Vec<_>>().concat(); // takes the different sets of words and puts them together to a string
+        let text = element.text().collect::<Vec<_>>().concat(); 
         
-        results.push(crates::json_download::ResultItem { tag, text }); // pushes the data into the public struct in save_to_json
+        results.push(crates::json_download::ResultItem { tag, text });
     }
 
-    crates::json_download::save_to_json(&results, "results.json")?; // saves the scraped data to results.json
+    crates::json_download::save_to_json(&results, "results.json")?;
 
     println!("Results saved at results.json");
 
@@ -39,18 +39,3 @@ fn main() -> Result<(), Box<dyn Error>> {
     io::stdin().read_line(&mut input)?;
     Ok(())
 }
-
-
-/* 
-TODO
-- Make it go on other links and scrape them too
-- Make it parse the text in the json nicer
-- Add more features
-
-
-GIT
-- git status
-- git add .
-- git commit -m "comment"
-- git push origin main
-*/
